5f9be7684f5e0b2db7f1b1abdada3a74fce01a61
Stephen Daimler <stephen@packlate.com>
2012-08-15 10:47:58 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickbooksApp::Application.config.secret_token = '2e53263939d6c3eeddae55a9d9374ad2eecdc68144e7707a0aa258749aed259f90df6d39cfd2672a4c11aff3bd5575acaa36a226cdc80499daa31941d9eaee51'
